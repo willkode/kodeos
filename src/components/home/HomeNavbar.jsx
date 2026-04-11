7ca8d6dddb2ae4a74e7efb4489933af4d0e4590a
Will Kode <iamwillkode@gmail.com>
2026-04-11 17:01:03 +0000

    File changes

--- src/components/home/HomeNavbar.jsx
+++ src/components/home/HomeNavbar.jsx
@@ -8,7 +8,7 @@ export default function HomeNavbar({ user }) {
 
   return (
     <nav className="fixed top-0 left-0 right-0 z-50 border-b border-white/[0.06] bg-[#09090B]/80 backdrop-blur-xl">
-      <div className="max-w-6xl mx-auto px-6 h-16 flex items-center justify-between">
+      <div className="max-w-7xl mx-auto px-6 h-16 flex items-center justify-between">
         <div className="cursor-pointer" onClick={() => navigate('/')}>
           <GlitchLogo text="KODEOS" />
         </div>
